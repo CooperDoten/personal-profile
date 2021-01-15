88a42073cf933caa9e6d90b0d3a089f21094c59e
Cooper Doten <cooperdoten@Coopers-MacBook-Pro.local>
2021-01-15 15:06:21 -0600

    adding some styling to music and projects area

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,13 +9,11 @@ class Footer extends Component {
   render() {
     return (
         <div className="Footer">
-        <div className="Footer-Bar">
           <div className="Footer-Options">
             <h1 className="Nav-Links" id="copyright">&#169; Cooper Doten</h1>
             <a className="Nav-Logos" href="https://github.com/CooperDoten"><img  id="gitIcon" src="/github-logo.png" alt="github logo"/></a>
             <a className="Nav-Logos" href="https://www.linkedin.com/in/cooper-doten/"><img  id="linkedIcon" src="/linkedInLogo.png" alt="linkedIn logo"/></a>
           </div>
-        </div>
       </div>
     );
   }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
 import './Projects.css'
-import Flipper from '../Flipper/Flipper';
-import FlipperTwo from '../FlipperTwo/FlipperTwo';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 class Projects extends Component {
   state = {
     heading: 'Projects',
+ 
   };
   componentDidMount() {
     console.log(`testing... testing... is this thing on?`)
   }
  
   render() {     
-    return (        
+    return (   
       <div className="Projects">
-
+        <Carousel>
+          <div>
+              <img src="/MNGO.png" />
+             <a href="https://github.com/CooperDoten/MN-GO"><p className="legend">MN GO</p></a> 
+          </div>
+          <div>
+              <img src="/blackIgnite.png" />
+              <a href="https://github.com/CooperDoten/black-ignite"><p className="legend">Black Ignite!</p></a> 
+          </div>
+          <div>
+              <img src="/movie_saga_homepage_screenshot.png" />
+              <a href="https://github.com/CooperDoten/movie-sagas"><p className="legend">Movie Saga</p></a> 
+          </div>
+          <div>
+              <img src="toDoList.png" />
+              <a href="https://github.com/CooperDoten/weekend-sql-to-do-list"><p className="legend">Weekend To Do List</p></a> 
+          </div>
+          <div>
+              <img src="CoopsGallery.png" />
+              <a href="https://github.com/CooperDoten/react-gallery"><p className="legend">React Gallery</p></a> 
+          </div>
+          </Carousel>
+      <div className="videos">
         <iframe className="vimeo"
-            src="https://player.vimeo.com/video/474486293" 
-            title="Minnesota Go project presentation"
-            frameBorder="0" 
-            allow="autoplay; fullscreen" 
-            allowFullScreen>
+          src="https://player.vimeo.com/video/474486293" 
+          title="Minnesota Go project presentation"
+          frameBorder="0" 
+          allow="autoplay; fullscreen" 
+          allowFullScreen>
         </iframe>
       </div>
+    </div>
     );
   }
 }
